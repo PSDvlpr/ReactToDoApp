dfbbadacca226091ea5c0fadf2c20bd3dc0be80e
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-03-09 14:39:43 +0100

    Change Form to react class

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
-const TodoForm = props => {
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);
+}
+
+updateTodo(e) {
+  e.preventDefault();
+  this.setState({
+    text: e.target.value
+  });
+}
+
+handleSubmit(e) {
+  e.preventDefault();
+  this.props.addTodo(this.state.text);
+  this.setState ({
+    text: ''
+  });
+}
+
+
+render() {
     return(
-      <form onSubmit={props.addTodo.bind(this, props.inputValue)}>
+      <form onSubmit={this.handleSubmit}>
           <input
               type="text"
               placeholder="Add Todo"
-              value={props.inputValue}
-              onChange={props.updateTodo} />
+              value={this.state.text}
+              onChange={this.updateTodo} />
           <button
             type="submit">Add Todo
           </button>
       </form>
     );
   }
+}
 
 export default TodoForm;
